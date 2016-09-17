953a6d16c096875bbac95bd27ea48c136e75b871
Evan Gui <evan176@hotmail.com>
2016-09-17 15:59:58 +0800

    Add __init__.py for import nn packages

---
+++ nn/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .neuron import decision, activation, output
+from .cost import loss
+from .backprop import calculate_cursis, calculate_gradient
+from .network import NeuralNetwork 
