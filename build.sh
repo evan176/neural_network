c742874b92118d5122bfcb99e30772a7bdb5a832
Evan Gui <evan176@hotmail.com>
2016-10-04 09:27:38 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -32,10 +32,13 @@ function project_test {
 
 
 function project_clean {
-  deactivate
+  find . -name '*.pyc' -exec rm --force {} +
+  find . -name '*.pyo' -exec rm --force {} +
+  find . -name '*~' -exec rm --force {} +
+  rm --force --recursive build/
+  rm --force --recursive dist/
+  rm --force --recursive *.egg-info
   rm -rf local
-  rm neural_network/*.pyc
-  rm tests/*.pyc
 }
 
 
